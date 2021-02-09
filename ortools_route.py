a2000d33987f49c47debb7c2ec34bbd1730ca10b
Tabbomat <tabbe123@googlemail.com>
2021-02-09 12:16:46 +0100

    add some lru_cache to mad_api

--- ortools_route.py
+++ ortools_route.py
@@ -5,7 +5,7 @@ import numpy as np
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
 
-import utility.mad_api
+import utility.mad_api as mad_api
 
 
 def distance_matrix(coords):
@@ -79,38 +79,38 @@ if __name__ == '__main__':
     # TODO: GET /recalc_status yields a list [id_1, id_2, id_2] of areas whose routes are still recalculating
     # TODO: ask user if they want to first recalc using MAD. recalc them. wait. If an area finished recalculating, open a separate thread which or_recalcs the route
     # fetch areas
-    areas = utility.mad_api.get_areas()
+    areas = mad_api.get_areas()
     for area_id, area_name in areas.items():
         print(f'{area_id[area_id.rfind("/") + 1:]:>4}: {area_name}')
     areas_to_recalc = input("Please input area ids of areas you want to recalculate separated by comma, or 'all' to recalc all areas:\n")
     # check if we want to recalc all areas or only specific ones
     if areas_to_recalc.lower().strip(' \t\r\n\'"') == 'all':
-        area_ids = [int(area_id[area_id.rfind('/') + 1:]) for area_id in areas.keys()]
+        area_ids = mad_api.get_area_ids()
     else:
         area_ids = list(map(int, areas_to_recalc.split(',')))
     do_mad_recalc = input("Do you want to trigger a MAD recalc before using ortools to calculate the routes? (Y/n) ").strip().lower() != 'n'
     for i, area_id in enumerate(area_ids):
-        area = utility.mad_api.get_area(area_id)
+        area = mad_api.get_area(area_id)
         print(f"Recalculating route for area {area['name']} ({i + 1}/{len(area_ids)})")
         if do_mad_recalc:
             print(f"Using MAD to recalculate route")
-            utility.mad_api.mad_recalc_area(area_id)
-            areas_in_recalculation = utility.mad_api.get_recalc_status()
+            mad_api.mad_recalc_area(area_id)
+            areas_in_recalculation = mad_api.get_recalc_status()
             if area_id not in areas_in_recalculation:
                 # either recalculation was incredibly quick, or it hasn't been queued yet
                 for _ in range(5):
                     time.sleep(1)
-                    areas_in_recalculation = utility.mad_api.get_recalc_status()
+                    areas_in_recalculation = mad_api.get_recalc_status()
                     if area_id in areas_in_recalculation:
                         break
                 # at this point, area_id should be in areas_in_recalculation. if it is not, then the area was super quickly recalculated and we just wasted 5 seconds
             while area_id in areas_in_recalculation:
                 time.sleep(1)
-                areas_in_recalculation = utility.mad_api.get_recalc_status()
+                areas_in_recalculation = mad_api.get_recalc_status()
         # get old routecalc
         routecalc_id = int(area['routecalc'].split('/')[-1])
         mode = area['mode']
-        old_route = utility.mad_api.get_routecalc_routefile(routecalc_id)
+        old_route = mad_api.get_routecalc_routefile(routecalc_id)
         # check if area has a route (otherwise we can't and don't need to recalculate)
         if not old_route:
             print("Area has no route.")
@@ -120,7 +120,7 @@ if __name__ == '__main__':
             print("Using ortools to recalculate route")
         new_route = recalc_routecalc(old_route, mode == 'pokestops')
         # set new route
-        utility.mad_api.set_routecalc_routefile(routecalc_id, new_route)
+        mad_api.set_routecalc_routefile(routecalc_id, new_route)
     # apply settings if desired
     if input("Do you want to apply the changes and reload? (y/N)  ").strip().lower() == 'y':
-        utility.mad_api.apply_settings()
+        mad_api.apply_settings()
--- utility/mad_api.py
+++ utility/mad_api.py
@@ -1,3 +1,4 @@
+import functools
 from typing import Dict, List
 
 import requests
@@ -9,15 +10,24 @@ MAD_URL = ARGS['madmin_url']
 MAD_AUTH = (ARGS['madmin_user'], ARGS['madmin_password']) if ARGS['madmin_user'] else None
 
 
+# some_api_function.cache_clear() clears the cache, if necessary
+
 def apply_settings():
     requests.get(MAD_URL + '/reload', auth=MAD_AUTH)
 
 
+@functools.lru_cache()
 def get_areas() -> Dict[str, str]:
     return requests.get(MAD_URL + '/api/area', auth=MAD_AUTH).json().get('results')
 
 
-def get_area(area_id: int):
+@functools.lru_cache()
+def get_area_ids() -> List[int]:
+    return [int(area_id[area_id.rfind('/') + 1:]) for area_id in get_areas().keys()]
+
+
+@functools.lru_cache()
+def get_area(area_id: int) -> dict:
     return requests.get(f'{MAD_URL}/api/area/{area_id}', auth=MAD_AUTH).json()
 
 
@@ -37,10 +47,37 @@ def get_recalc_status() -> List[int]:
     return requests.get(f'{MAD_URL}/recalc_status', auth=MAD_AUTH).json()
 
 
+@functools.lru_cache()
 def get_geofence(geofence_id: int) -> dict:
     return requests.get(f'{MAD_URL}/api/geofence/{geofence_id}', auth=MAD_AUTH).json()
 
 
+@functools.lru_cache()
 def get_spawnpoints(area_id: int, index: int = 0, event_id: int = 1) -> List[dict]:
     # result is list of dicts like {"id":123456,"lastnonscan":"2020-12-15 18:43:37","lastscan":"1970-01-01 00:00:00","lat":12.345,"lon":56.789}
     return requests.get(f'{MAD_URL}/get_spawn_details', params={'area_id': area_id, 'event_id': event_id, 'mode': 'ALL', 'index': index}, auth=MAD_AUTH).json()
+
+
+@functools.lru_cache()
+def get_all_spawnpoints(area_id: int, event_id: int = 1) -> List[dict]:
+    # similar to get_spawnpoints, but joins data over all possible index values
+    result = []
+    for index in range(get_number_of_geofences(get_geofence_included_id(area_id))):
+        result.extend(get_spawnpoints(area_id, index, event_id))
+    return result
+
+
+@functools.lru_cache()
+def get_geofence_included_id(area_id: int) -> int:
+    area = get_area(area_id)
+    if 'geofence_included' in area.keys():
+        geofence_included = area['geofence_included']  # type:str
+        return int(geofence_included[geofence_included.rfind('/') + 1:])
+    return -1
+
+
+@functools.lru_cache()
+def get_number_of_geofences(geofence_id: int) -> int:
+    geofence = get_geofence(geofence_id)
+    # each geofence starts with a line containing [name]
+    return sum(1 for line in geofence['fence_data'] if line[0] == '[' and line[-1] == ']')
